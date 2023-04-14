0525650f09de1bd298f5a87ce01969e219a0c590
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-04-14 19:28:03 -0300

    finish

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,16 +1,30 @@
-import react from 'react';
+import react, { useState } from 'react';
 import Tags from '../Tags';
 import style from './Galeria.module.scss';
 import fotos from "./fotos.json";
 import Cards from './Cards';
 
 const Galeria = () => {
+    const [itens, setItens] = useState(fotos);
+
+    const tags = [...new Set(fotos.map(foto => foto.tag))];
+
+    const filtraFotos=(tag) => {
+        const novasFotos = fotos.filter(foto => foto.tag === tag);
+
+        setItens(novasFotos);
+    }
+
+
     return (
         <section className={style.galeria}>
             <h2>Navegue pela Galeria</h2>
-            <Tags/>
+            <Tags 
+            tags={tags}
+            filtraFotos={filtraFotos}
+            setItens={setItens}/>
             <Cards
-            fotos={fotos} 
+            fotos={itens} 
             styles={style}/>
         </section>
     )
--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,14 +1,14 @@
 import style from './Tags.module.scss'
+import fotos from '../Galeria/fotos.json';
 
-const Tags = () => {
+const Tags = ({tags, filtraFotos, setItens}) => {
+    
     return (
         <div className={style.tags}>
             <p>Filtre as Tags:</p>
             <ul className={style.tags__lista}>
-                <li>Estrelas</li>
-                <li>Galáxias</li>
-                <li>Lua</li>
-                <li>Planetas</li>
+                {tags.map(tag =><li key={tag} onClick={()=>filtraFotos(tag)}>{tag}</li>)}
+                <li onClick={()=>setItens(fotos)}>Todas</li>
             </ul>
         </div>
     )
